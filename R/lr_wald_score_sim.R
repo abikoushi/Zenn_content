54559c33f9afb65550adeeb8f5ca8d376c833097
abikoushi <a.b.e.k.o.69@gmail.com>
2025-08-25 20:29:35 +0900

    update size of fonts in Figure in 'LR_score_wald'

--- R/lr_wald_score_sim.R
+++ R/lr_wald_score_sim.R
@@ -33,15 +33,18 @@ set.seed(1234)
 system.time({
   res1 = lapply(seq_len(nrow(sim_settings)), function(i){simfunc(p_true=sim_settings$p_true[i], p_test=sim_settings$p_true[i], n=sim_settings$n[i], 10000)})
 })
-#     ユーザ システム     経過 
+#  ユーザ システム     経過 
 #  21.995    0.057   22.104
 # saveRDS(res1, file="result_binom_sim1.rds")
+# res1 <- readRDS(file="result_binom_sim1.rds")
 
 res1_df = pivot_longer(bind_rows(res1), lr:score_c,  values_to = "p_value", names_to = "method")
 
 CP_df = group_by(res1_df, method, p_true, n)%>%
   summarise(cp = mean(p_value > 0.05))
 CP_df
+
+
 
 p_cp = ggplot(CP_df,aes(x=p_true, y=cp, colour=method, linetype=method, shape=method))+
   geom_hline(yintercept =  0.95, colour = "darkgrey")+
@@ -50,7 +53,8 @@ p_cp = ggplot(CP_df,aes(x=p_true, y=cp, colour=method, linetype=method, shape=me
   labs(y="coverage probability")+
   facet_grid(rows = vars(n), labeller = label_both)+
   scale_color_brewer(palette = "Set2")+
-  theme_bw()+theme(strip.text.y = element_text(angle=0),strip.background = element_rect(fill = "white"))
+  theme_bw(18) + 
+  theme(strip.text.y = element_text(angle=0),strip.background = element_rect(fill = "white"))
 print(p_cp)
 ggsave("p_cp.png", plot = p_cp, width = 10, height = 10)
 
@@ -60,7 +64,10 @@ p = ggplot(res1_df, aes(colour=method, linetype=method)) +
   facet_grid(p_true~n, labeller = label_both)+
   labs(x="nominal", y="actual")+
   scale_color_brewer(palette = "Set2")+
-  theme_bw()+theme(strip.text.y = element_text(angle=0),strip.background = element_rect(fill = "white"))
+  theme_bw(18) + 
+  theme(axis.text.x = element_text(angle = 90),
+        strip.text.y = element_text(angle=0), 
+        strip.background = element_rect(fill = "white"))
 print(p)
 ggsave("p_alpha.png", plot = p, width = 10, height = 10)
 
