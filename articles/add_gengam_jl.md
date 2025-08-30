787c4a5345cfb3a02e60f8a68bc794ed4b4d50f4
abikoushi <a.b.e.k.o.69@gmail.com>
2025-08-30 09:19:32 +0900

    shitagaki ggamma

--- articles/add_gengam_jl.md
+++ articles/add_gengam_jl.md
@@ -1,29 +1,75 @@
 ---
-title: "Distributions.jl に自分で定義した分布を追加する；一般化ガンマ分布を例に"
+title: "Distributions.jl を利用して確率分布を宣言する（一般化ガンマ分布の例）"
 emoji: "⛑️"
 type: "tech" # tech: 技術記事 / idea: アイデア
-topics: [Julia]
+topics: [Julia, R]
 published: false
 ---
 
+## 主にやったこと
 
-次の密度関数 $f(x;a,b,k)$ を持つ確率分布を一般化ガンマ分布（[Generalized gamma distribution - wikipedia](https://en.wikipedia.org/wiki/Generalized_gamma_distribution)）という.
+次の分布関数 $f(x;a,b,k)$ を持つ確率分布を一般化ガンマ分布（[Generalized gamma distribution - wikipedia](https://en.wikipedia.org/wiki/Generalized_gamma_distribution)）という.
+
+$$
+F(x; a, b, k) = \frac{\gamma(a/k, (x/b)^k)}{\Gamma(a/k)}, \quad x > 0. \tag{1}
+$$
+
+ここで $\gamma(a,x)$ は第1種不完全ガンマ関数（ [不完全ガンマ関数 - wikipedia](https://ja.wikipedia.org/wiki/不完全ガンマ関数) ），パラメータ $(a, b, k)$ はすべて正の値を取る．密度関数は次の通り．
 
 $$
 f(x; a, b, k) = \frac{(k/b^a) x^{a-1} \exp(-(x/b)^k)}{\Gamma(a/k)}, \quad x > 0.
 $$
 
-$(a, b, k)$ はすべて正の値を取るパラメータである．Distributions.jl はいい感じのパッケージだが，幸か不幸か，一般化ガンマ分布は用意されていないようなので （ [Distributions.jl; Univariate Continuous Distributions](https://juliastats.org/Distributions.jl/stable/univariate/#Continuous-Distributions) ）これを実装してみた．具体的には，次のようにした．
+`Distributions.jl` はいい感じのパッケージだが，幸か不幸か，一般化ガンマ分布は用意されていないようなので （ [Distributions.jl - Univariate Continuous Distributions](https://juliastats.org/Distributions.jl/stable/univariate/#Continuous-Distributions) ）これを実装してみた．具体的には，以下のコードのようにした．
 
-あとは答えあわせのためにやったことをメモしておく．
+https://github.com/abikoushi/Zenn_content/blob/main/jl/add_gengam_jl.jl
 
-パラメータ $(a, b, k)$ の値をいくつか選んでプロットしたものが下の図である.
+- 1行目から95行目：追加したい機能を `module` ... `end` で囲んでいます．こうしておくと他の関数と衝突したりしないし，「やっぱりこの書き方やめた！」となったとき上書きできるので試行錯誤段階からこうしておくほうが便利です．
+- 25行目：`gamma_inc` は全区間での積分が1になるよう正規化された不完全ガンマ関数……つまり(1)式の分母の部分も含めて定義されています．戻り値の1つめが第1種不完全ガンマ関数（下側），2つめが第2種不完全ガンマ関数（上側）です．
+- 75行目から78行目：乱数生成 `rand` は逆関数法によります．
+- 88行目から91行目：対数の平均（≠平均の対数）を求める関数を「幾何平均の対数」というニュアンスを込めて `loggeommean` という名前で宣言しています．
+- 96行目以降：正しく実装できているか答えあわせのためにやっています．下の解説に続きます．
 
-確率変数 $X$ が一般化ガンマ分布に従うことを $X \sim \mathrm{GG}(a, b, k)$ と書くことにする.
+`RCall.jl` を使って R を呼び，R の実装と比較して答えあわせとする．ただし R の `ggamma` パッケージではパラメトライズが (1) と少し異なり，こんなふうだ．
 
-## 付録：期待値と対数の期待値
+$$
+F(x; a, b, k) = \frac{\gamma(k, (x/a)^k)}{\Gamma(k)}, \quad x > 0.
+$$
 
-$X \sim \mathrm{GG}(a, 1, k)$ の場合の期待値 $E[X]$ を定義通り計算すると次のようになる．
+下記の箇条書きのように置き換えると一致する．
+
+- $a \leftarrow b$
+- $b \leftarrow k$
+- $k \leftarrow k/a$
+
+すなわち，こんなふうにした．
+
+```r
+dggamma2 = function(x,a,b,k){ggamma::dggamma(x,b,k,a/k)}
+```
+
+パラメータ $(a, b, k)$ の値をいくつか選んで密度関数をプロットしたものが下の図だ.
+
+![](/images/add_gengam_jl/dggamma.png)
+
+R の実装（なめらかな曲線）と Julia の実装（点）で値が一致していることがわかる．
+
+疑似乱数の経験分布と真の分布関数を比較したのが下の図．
+
+![](/images/add_gengam_jl/ecdf_ggamma.png)
+
+平均（`mean`）と対数の平均（`loggeommean`）については次のように標本平均と近い値が得られていることを見ている．
+
+```julia
+mean(x) #6.012540422658787
+GG.mean(ggdist1) #6.0
+mean(log, x) #0.848834299052846
+GG.loggeommean(ggdist1) #0.8455686701969344
+```
+
+## 付録：一般化ガンマ分布の期待値，対数の期待値
+
+確率変数 $X$ が一般化ガンマ分布に従うことを $X \sim \mathrm{GG}(a, b, k)$ と書くことにする. $X \sim \mathrm{GG}(a, 1, k)$ の場合の期待値 $E[X]$ を定義通り計算すると次のようになる．
 
 $$
 \begin{aligned}
@@ -88,5 +134,10 @@ $$
 
 である.
 
-$E[\log(bX)]=E[\log(X)]+\log(b)$ なので, $X \sim \mathrm{GG}(a, b, k)$ のときは $E[\log(X)]=\psi(a/k)/k+\log(b)$.
+$E[\log(bX)]=E[\log(X)]+\log(b)$ なので， $X \sim \mathrm{GG}(a, b, k)$ のときは 
 
+$$
+E[\log(X)]=\psi(a/k)/k+\log(b) .
+$$
+
+（おしまい）
--- jl/add_gengam_jl.jl
+++ jl/add_gengam_jl.jl
@@ -91,53 +91,144 @@ function loggeommean(d::GeneralizedGamma)
 end
 
 export GeneralizedGamma
-export cdf, ccdf, pdf, logpdf, mean, meanlog, shape, scale, power, params, rand
+export cdf, ccdf, pdf, logpdf, mean, loggeommean, shape, scale, power, params, rand
 end
 
+using StatsBase
 using Statistics
 using Plots
 using Random
 using RCall
+using DataFrames
+
+ggdist1 = GG.GeneralizedGamma(1,1,0.5)
+ggdist2 = GG.GeneralizedGamma(1,2,2)
+ggdist3 = GG.GeneralizedGamma(2,0.1,0.5)
+ggdist4 = GG.GeneralizedGamma(2,1,2)
+ggdist5 = GG.GeneralizedGamma(1,2.5,5)
 
 R"
 library(ggamma)
-curve(dggamma(x,1,1,0.5),0,8)
+library(ggplot2)
+library(patchwork)
+dggamma2 = function(x,a,b,k){dggamma(x,b,k,a/k)}
 "
 
-plot(x-> GG.pdf(GG.GeneralizedGamma(1,1,0.5), x),0, 8, label = "(1,1,0.5)")
-plot!(x-> GG.pdf(GG.GeneralizedGamma(1,2,2), x), label = "(1,2,2)")
-plot!(x-> GG.pdf(GG.GeneralizedGamma(2,1,0.5), x), label = "(2,1,0.5)")
-plot!(x-> GG.pdf(GG.GeneralizedGamma(2,1,2), x), label = "(2,1,2)")
-plot!(x-> GG.pdf(GG.GeneralizedGamma(1,5,5), x), label = "(1,5,5)")
-png("gr.png")
+x = 0.01:0.1:15
+dens = GG.pdf(ggdist1, x)
+df = DataFrame(x=x, y=dens)
 
+R"
+p1 = ggplot($df, aes(x, y))+
+  geom_point()+
+  stat_function(fun = dggamma2, args = list(a=1, b=1, k=0.5), n=1001)+
+  ggtitle('(a=1, b=1, k=0.5)')+
+  theme_bw()
+"
+
+dens = GG.pdf(ggdist2, x)
+df = DataFrame(x=x, y=dens)
+
+R"
+p2 = ggplot($df, aes(x, y))+
+  geom_point()+
+  stat_function(fun = dggamma2, args = list(a=1, b=2, k=2))+
+  ggtitle('(a=1, b=2, k=2)')+
+  theme_bw()
+"
+
+dens = GG.pdf(ggdist3, x)
+df = DataFrame(x=x, y=dens)
+
+R"
+p3 = ggplot($df, aes(x, y))+
+  geom_point()+
+  stat_function(fun = dggamma2, args = list(a=2, b=0.1, k=0.5))+
+  ggtitle('(a=2, b=0.1, k=0.5)')+
+  theme_bw()
+"
+
+dens = GG.pdf(ggdist4, x)
+df = DataFrame(x=x, y=dens)
+
+R"
+p4 = ggplot($df, aes(x, y))+
+  geom_point()+
+  stat_function(fun = dggamma2, args = list(a=2, b=1, k=2))+
+  ggtitle('(a=2, b=1, k=2)')+
+  theme_bw()
+"
+
+dens = GG.pdf(ggdist5, x)
+df = DataFrame(x=x, y=dens)
+
+R"
+p5 = ggplot($df, aes(x, y))+
+  geom_point()+
+  stat_function(fun = dggamma2, args = list(a=1, b=2.5, k=5))+
+  ggtitle('(a=1, b=2.5, k=5)')+
+  theme_bw()
+"
+
+R"
+p = p1+p2+p3+p4+p5
+ggsave('dggamma.png', plot=p, width=8, height=10)
+"
+
+
+##
 rng1 = Random.default_rng(1)
-x = GG.rand(rng1,GG.GeneralizedGamma(1,1,0.5),100000)
-mean(x)
-GG.mean(GG.GeneralizedGamma(1,1,0.5))
-mean(log, x)
-GG.loggeommean(GG.GeneralizedGamma(1,1,0.5))
 
-GG.rand!(rng1,GG.GeneralizedGamma(1,2,2), x)
-mean(x)
-GG.mean(GG.GeneralizedGamma(1,2,2))
-mean(log, x)
-GG.loggeommean(GG.GeneralizedGamma(1,2,2))
+x = GG.rand(rng1, ggdist1, 100000)
+p1 = plot(y -> ecdf(x)(y), 0, 10, linestyle=:dash, label="ecdf")
+plot!(p1, y -> GG.cdf(ggdist1,y), linestyle=:dashdot, label="true")
+title!(p1, "(a=1,b=1,k=0.5)")
 
-GG.rand!(MersenneTwister(1),GG.GeneralizedGamma(2,1,0.5), x)
-mean(x)
-GG.mean(GG.GeneralizedGamma(2,1,0.5))
-mean(log, x)
-GG.meanlog(GG.GeneralizedGamma(2,1,0.5))
+mean(x) #6.012540422658787
+GG.mean(ggdist1) #6.0
+mean(log, x) #0.848834299052846
+GG.loggeommean(ggdist1) #0.8455686701969344
 
-GG.rand!(MersenneTwister(1),GG.GeneralizedGamma(2,1,2), x)
-mean(x)
-GG.mean(GG.GeneralizedGamma(2,1,2))
-mean(log, x)
-GG.meanlog(GG.GeneralizedGamma(2,1,2))
+GG.rand!(rng1, ggdist2, x)
+p2 = plot(y -> ecdf(x)(y), 0, 10, linestyle=:dash, label="ecdf")
+plot!(p2, y -> GG.cdf(ggdist2,y), linestyle=:dashdot, label="true")
+title!(p2, "(a=1,b=2,k=2)")
 
-GG.rand!(MersenneTwister(1),GG.GeneralizedGamma(1,5,5), x)
 mean(x)
-GG.mean(GG.GeneralizedGamma(1,5,5))
+GG.mean(ggdist2)
 mean(log, x)
-GG.meanlog(GG.GeneralizedGamma(1,5,5))
+GG.loggeommean(ggdist2)
+
+
+GG.rand!(rng1, ggdist3, x)
+p3 = plot(y -> ecdf(x)(y), 0, 10, linestyle=:dash, label="ecdf")
+plot!(p3, y -> GG.cdf(ggdist3,y), linestyle=:dashdot, label="true")
+title!(p3, "(a=2,b=0.1,k=0.5)")
+
+mean(x) #1.9975355185928443
+GG.mean(ggdist3) #2.0
+mean(log, x)
+GG.loggeommean(ggdist3)
+
+
+GG.rand!(rng1, ggdist4, x)
+p4 = plot(y -> ecdf(x)(y), 0, 10, linestyle=:dash, label="ecdf")
+plot!(p4, y -> GG.cdf(ggdist4, y), linestyle=:dashdot, label="true")
+title!(p4, "(a=2,b=1,k=2)")
+mean(x)
+GG.mean(ggdist4)
+mean(log, x)
+GG.loggeommean(ggdist4)
+
+GG.rand!(rng1, ggdist5, x)
+p5 = plot(y -> ecdf(x)(y), 0, 10, linestyle=:dash, label="ecdf")
+plot!(p5, y -> GG.cdf(ggdist5, y), linestyle=:dashdot, label="true")
+title!(p5, "(a=1,b=2.5,k=5)")
+
+mean(x)
+GG.mean(ggdist5)
+mean(log, x)
+GG.loggeommean(ggdist5)
+
+p = plot(p1,p2,p3,p4,p5, size=(1000,800))
+savefig("ecdf_ggamma.png")
