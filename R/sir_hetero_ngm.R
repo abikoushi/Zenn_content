d04f6786f76d4fcf97490a0c5db10f4212e429e3
KoAbe <a.b.e.k.o.69@gmail.com>
2026-09-09 01:39:07 +0900

    touch sir_hetero_ngm

--- R/sir_hetero_ngm.R
+++ R/sir_hetero_ngm.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(deSolve)
+library(nleqslv)
 
 sol_ode_from_states <- function(df_state, times, func, parms){
   res_df <- lapply(1:nrow(df_state), function(i){
@@ -15,11 +16,12 @@ SIRmod <- function(Time, State, Pars) {
   k <- Pars$k
   beta <- Pars$beta
   gamma <- Pars$gamma
+  N <- Pars$N
   S <- State[1:k]
   I <- State[(k+1):(2*k)]
   R <- State[(2*k+1):(3*k)]
-  dS <- - drop(beta%*%I)*S
-  dI <- drop(beta%*%I)*S - gamma*I
+  dS <- - drop(beta%*%I)*S/N
+  dI <- drop(beta%*%I)*S/N - gamma*I
   dR <- gamma*I
   return(list(c(dS, dI, dR)))
 }
@@ -28,38 +30,42 @@ SIRmod <- function(Time, State, Pars) {
 #                  0.2, 0.2, 0.1,
 #                  0.2, 0.1, 0.1), byrow = TRUE, nrow = 3)
 
-set.seed(20260908); beta <- matrix(rexp(9,10), byrow = TRUE, nrow = 3)
+set.seed(20260909); beta <- matrix(rexp(4,1), byrow = TRUE, nrow = 2)
 print(beta)
 
-pars  <- list(beta = beta, gamma = 0.1, k=3)
-times <- seq(0, 150, by = 0.1)
+N <- c(1, 1)
+pars  <- list(beta = beta, gamma = 0.1, N=N, k=2)
+times <- seq(0, 100, by = 0.1)
 
-ini = c(S1=1, S2=0.999, S3=1,
-        I1=0, I2=0.001, I3=0,
-        R1=0, R2=0, R3=0)
+# ini = c(S1=1, S2=0.999, S3=1,
+#         I1=0, I2=0.001, I3=0,
+#         R1=0, R2=0, R3=0)
+
+ini = c(S1=1, S2=0.999,
+        I1=0, I2=0.001,
+        R1=0, R2=0)
 
 ode_out <- ode(y=ini, times=times, func=SIRmod, parms=pars)
 
-
 sir_out <- data.frame(ode_out) |> 
-  pivot_longer(S1:R3) |> 
+  pivot_longer(S1:R2) |> 
   mutate(state = substr(name, 1,1), 
          subgroup = substr(name, 2,2)) |> 
   mutate(state = factor(state, levels = c("S","I","R")))
 
-col3 <- hcl.colors(3, palette = "Set2")
+col2 <- hcl.colors(2, palette = "Set2")
 ggplot(sir_out, aes(x = time, y = value,
                     colour = subgroup, 
                     linetype = subgroup))+
   geom_line() + 
   facet_grid(row=vars(state)) + 
-  scale_color_manual(values = col3)+
+  scale_color_manual(values = col2)+
   theme_bw(15)
 
 ####
 #exp growth
 ####
-A <- pars$beta + diag(-pars$gamma,3)
+A <- pars$beta + diag(-pars$gamma, pars$k)
 eiA <- eigen(A)
 
 matexp = function(t, P, Pinv, values, yini){
@@ -72,79 +78,39 @@ Pinv_A = solve(ei_A$vectors)
 
 sol0 = sapply(times, matexp, 
               P=P_A, Pinv=Pinv_A, values=ei_A$values,
-              yini=ini[4:6])
-row.names(sol0) <- names(ini[4:6])
+              yini=ini[3:4])
+row.names(sol0) <- names(ini[3:4])
 exp_out <- data.frame(time=times, t(sol0)) |> 
-  pivot_longer(I1:I3)
+  pivot_longer(I1:I2)
 sir_out_inf <- dplyr::filter(sir_out, state=="I")
 
 ggplot(sir_out_inf, mapping = aes(x=time, y=value, group=name, colour=name))+
   geom_line()+
   geom_line(data = exp_out, linetype = 2, linewidth = 1)+
-  scale_colour_manual(values = c(col3, "grey"))+
+  scale_colour_manual(values = col2)+
   labs(colour = "state") +
   facet_grid(row=vars(name))+
   ylim(c(0,1.05)) +
   theme_bw(15)
 
 
-####
-#threshold
-####
-
-R <- pars$beta%*%diag(1/pars$gamma,3)
-ei_R <- eigen(R)
-ei_R$values
-
-df_state <- data.frame(S1=seq(0.01, 0.99, by=0.02)) |> 
-  mutate(I1=0.01) |> 
-  mutate(R1=1-(S1+I1)) |>
-  mutate(S2=S1,S3=S1,
-         I2=0,I3=0) |> 
-  mutate(R2=1-(S2+I2),
-         R3=1-(S3+I3)) |>
-  dplyr::select(S1,S2,S3,I1,I2,I3,R1,R2,R3)
-
-
-res1 <- sol_ode_from_states(df_state=df_state, 
-                            times = times,
-                            func=SIRmod, parms=pars)
-
-res1 <- group_by(res1, group) |>  
-  mutate(iniS1 = first(S1))
-
-ggplot(data=res1, aes(x=time, y=I1+I2+I3, group = group, colour=iniS1)) +
-  geom_line()+
-  scale_colour_viridis_c()+
-  theme_bw() + labs(colour="S(0)")
-
-
-res2 <- sol_ode_from_states(df_state=df_state, 
-                            times = seq(0, 1, by=0.01),
-                            func=SIRmod, parms=pars)
-
-res2 <- group_by(res2, group) |>  
-  mutate(iniS1 = first(S1))
-
-ggplot(data=res2, aes(x=iniS1, y=I1+I2+I3, group = group, colour=time)) +
-  geom_line(arrow = arrow(length = unit(3,"pt")))+
-  geom_vline(xintercept =  1/ei_R$values[1], linetype = 2)+
-  scale_colour_gradient2(low="grey80", high="steelblue")+
-  labs(x="S(0)", y="I(1)")+
-  theme_bw()
-
-
 #####
 #final size
 #####
 
+KL <- pars$beta%*%diag(1/pars$gamma, pars$k)
+ei_KL <- eigen(KL)
+
 sir_out_rem <- dplyr::filter(sir_out, grepl("^R",name))
 
+fz <- function(z){z + expm1(-KL%*%z)}
+res <- nleqslv(c(1,1), fz)
+df_z <- data.frame(z = res$x, 
+                   name = paste0("R",1:2))
+
 ggplot(sir_out_rem, aes(x = time, y = value, colour = subgroup))+
+  geom_hline(data = df_z,  aes(yintercept = z), linetype = 2)+
   geom_line() + 
   facet_grid(row=vars(name)) + 
-  scale_color_manual(values = col3)+
+  scale_color_manual(values = col2)+
   theme_bw(15)+ylim(c(0,1))
-
-
-optim(c(0,0,0), function(z){sum(abs(-expm1(-R%*%z)))})
--- articles/sir_hetero_ngm.md
+++ articles/sir_hetero_ngm.md
@@ -2,6 +2,127 @@
 title: "人口の異質性を加味した SIR モデルに基づく次世代行列の構築 "
 emoji: "👨‍👦‍👦"
 type: "tech" # tech: 技術記事 / idea: アイデア
-topics: [R]
+topics: [R, 疫学, 微分方程式, 線形代数]
 published: false
 ---
+
+## このノートについて
+
+[西浦博　編著『感染症流行を読み解く数理』（日本評論社）](https://www.nippyo.co.jp/shop/book/8827.html) の第１章の「人口の異質性を加味した SIR モデル」の節を，自分なりに数値例を補いながら読んでいくものです．この第１章の筆者は小林鉄郎，西浦博です．ただし，（当たり前かもしれませんが，）この文書の責任は私にあります．本文中のコードは R 言語です．
+
+## 人口の異質性を加味した SIR モデル
+
+人口に $n$ 個のサブグループがあるとして，$a=1,\ldots, n$ について次の連立微分方程式で表されるモデルを考える．
+
+$$
+\begin{aligned}
+\frac{d}{dt}S_a(t) &= -S_a(t) \sum_{b=1}^n \beta_{ab} I_b(t)\\
+\frac{d}{dt}I_a(t) &= S_a(t) \left(\sum_{b=1}^n \beta_{ab} I_b(t) \right) - \gamma I_b(t)\\
+\frac{d}{dt}R_a(t) &= \gamma I_a(t)
+\end{aligned} \tag{1}
+$$
+
+これは人口の異質性を加味した SIR モデルであり，SIRモデルには感受性者 $S$，感染期 $I$，回復・免疫保持 $R$ の3つの区画がある．各パラメータは次のような意味を持つ．
+
+- $\beta_{ab}$: サブグループ $b$ の感染期の個体によるサブグループ $a$ の個体への感染伝播率
+- $\gamma$: 回復率（$1/\gamma$ が平均感染期間）
+
+今回は $S_a(t)+I_a(t)+R_a(t)=1$ になるよう，サブグループごとに正規化されているとする．
+
+ $S(t) = (S_1(t),S_2(t), \ldots, S_n(t))'$, $I(t) = (I_1(t),I_2(t), \ldots, I_n(t))'$, $T(t) = (I_1(t),I_2(t), \ldots, R_n(t))'$， さらに $\beta = (\beta_{ab})$ とまとめて置く．
+
+$$
+\begin{aligned}
+\frac{d}{dt}S(t) &= -S(t) \circ \beta I(t)\\
+\frac{d}{dt}I(t) &= S(t) \circ \beta I(t) - \gamma I(t)\\
+\frac{d}{dt}R(t) &= \gamma I(t)
+\end{aligned}
+$$
+
+ここで $\circ$ はアダマール積（ベクトルの要素ごとの積）とした．
+
+
+
+## 次世代行列の導入
+
+$S_a(t) \approx 1$ ($a=1,\ldots g$) が成り立つ感染流行初期を考える．このとき，感染 $I$ についての方程式は次のようになる．
+
+$$
+\frac{d}{dt}I(t) = \beta I(t) - \gamma I(t)
+$$
+
+これを次のような行列の和の形に分解しよう．
+
+$$
+\frac{d}{dt}I(t) = (T + \Sigma)I(t).
+$$
+
+ここで $T$ は新規感染の発生に関する項に注目した係数である．
+
+$$
+T = \beta.
+$$
+
+また $\Sigma$ は新規感染以外の状態の変化（ここでは回復）に関する項のみに注目した係数である．
+
+$$
+\Sigma = \begin{pmatrix}
+ -\gamma &  0 & \cdots &  0\\
+0 &  -\gamma & \cdots &  0\\
+\vdots& \vdots  & \ddots &  \vdots\\
+0 &  0 & \cdots &  -\gamma\\
+\end{pmatrix}
+.
+$$
+
+初期の感染は時間 $t$ について指数関数 $\exp((T+\Sigma)t)$ で近似できる．この指数関数と先程の SIR モデルの解を重ねてみる．
+
+この分解に基づき行列 $K_L$ を次のように定義する．
+
+$$
+K_L = -T \Sigma^{-1}.
+$$
+
+$K_L$ は「感染者が各状態で過ごす平均時間」と「その状態にいるときに新規感染を発生させる速度」を掛け合わせることで，「1人の感染者が生み出す新規感染者の総数」を表している．基本再生算数は $K_L$ の最大固有値で定義できる．
+
+$K_L$ の要素を書き下すと，
+
+$$
+K_L = 
+\begin{pmatrix}
+\beta_{11}/\gamma &  \beta_{12}/\gamma & \cdots &  \beta_{1n}/\gamma\\
+\beta_{21}/\gamma &  \beta_{22}/\gamma & \cdots &  \beta_{2n}/\gamma\\
+\vdots&  \vdots & \ddots &  \vdots\\
+\beta_{n1}/\gamma &  \beta_{n2}/\gamma & \cdots &  \beta_{nn}/\gamma\\
+\end{pmatrix}.
+$$
+
+以降では $K_L$ の $(a,b)$ 成分を $R_{ab}$ と置く．
+
+## 最終規模方程式
+
+(1) の $R(t)$ についての方程式より，
+
+$$
+I_a(t) = \frac{1}{\gamma }\frac {d}{dt}R_a(t).
+$$
+
+これを (1) の $S_a(t)$ についての方程式に代入すると，次の方程式を得る．
+
+$$
+\frac{1}{S_a(t)} {\frac {d}{dt}}S_a(t)=-  \sum_{b=1}^n\frac{\beta_{ab}}{\gamma}\frac {d}{dt}R_a(t).
+$$
+
+$z_a = \lim_{t \to \infty}R_a(t)$ と置き，$\lim_{t \to \infty}S_a(t) = 1-z_a$ に注意して，両辺を区間 $[0, \infty)$ で積分することで次の方程式を得る．
+
+$$
+\log(1-z_a)= \sum_{b=1}^n\frac{\beta_{ab}}{\gamma} z_b.
+$$
+
+整理すると，
+
+$$
+z_a= 1- \exp\left(\sum_{b=1}^nR_{ab} z_b \right).
+$$
+
+この方程式を満たす $z_a$ が最終規模である．
